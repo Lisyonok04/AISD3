5ae5bca6d190e5040dd4343246c6ef4526b180c5
Mary <marybelka04@gmail.com>
2024-05-28 14:19:26 +0400

    base class

--- task.cpp
+++ task.cpp
@@ -9,17 +9,161 @@
 #include <algorithm>
 #include <string>
 
+const int INF = std::numeric_limits<int>::max();
+
 using namespace std;
 
-template <typename Vertex, typename Distance = double>
+
+//template <typename Vertex, typename Distance = double>
+//struct Vertexes {
+//	struct Edge {
+//		int _vert;
+//		Distance _distance;
+//	};
+//	int _index;
+//	Vertex _value;
+//	list<Edge> _edges;
+//	Vertexes(int index, Vertex value) : _index(index), _value(value) {};
+//};
+template <typename V, typename Distance = double>
 class Graph {
 private:
 	struct Edge {
 		int _vert;
-		Distance _value;
+		Distance _distance;
 	};
 	struct Vertex {
-		int _num;
-		Vertex _val;
-		list<Edge> _edge;
+		int _index;
+		V _value;
+		list<Edge> _edges;
+		//Vertex(int index, Vertex value) : _index(index), _value(value) {};
 	};
+	vector<Vertex> _graph;
+
+	Vertex& operator[](V& val) {
+		for (auto& vertex : _graph) {
+			if (vertex._value == val) {
+				return vertex;
+			}
+		}
+		throw invalid_argument("No such vertex");
+	}
+
+	void renum_after_delete(int start) {
+		size_t count = 0;
+		for (auto& i : _graph) {
+			i._index = count;
+			for (auto& edge : i._edges) {
+				if (edge._vert >= start)
+					edge._vert--;
+			}
+			count++;
+		}
+	}
+public:
+	Graph() = default;
+	~Graph() = default;
+
+	void print() const {
+		for (auto& vertex : _graph) {
+			cout << vertex._value << " " << vertex._index;
+			for (auto& edge : vertex._edges) {
+				cout << "-->" << "|to " << _graph[edge._vert]._value << " - dist " << edge._distance << "|";
+			}
+			cout << endl;
+		}
+	}
+	size_t size() const {
+		return _graph.size();
+	}
+	bool has_vertex(const V& v) const {
+		for (const auto& vertex : _graph)
+			if (vertex._value == v)
+				return true;
+		return false;
+	};
+	void add_vertex(V v) {
+		if (!has_vertex(v)) {
+			int new_num = _graph.size();
+			_graph.push_back({ new_num,  v });
+		};
+	};
+	bool remove_vertex(V v) {
+		try {
+			auto index = (*this)[v]._index;
+			for (auto& vertex : _graph) {
+				vertex._edges.remove_if([&](Edge& x) { return _graph[x._vert]._value == v; });
+			}
+			_graph.erase(std::remove_if(_graph.begin(), _graph.end(), [v](Vertex& ver)
+				{
+					return ver._value == v;
+				}
+			), _graph.end());
+			renum_after_delete(index);
+			return true;
+		}
+		catch (invalid_argument& e) {
+			return false;
+		}
+	};
+	std::vector<Vertex> vertices() const {
+		return _graph;
+	};
+
+	void add_edge(V from, V to, Distance d) {
+		try {
+			(*this)[from]._edges.push_back({ (*this)[to]._index, d });
+		}
+		catch (invalid_argument& e) {
+			std::cerr << e.what() << std::endl;
+		}
+	};
+	bool remove_edge(V from, V to) {
+		if (has_vertex(from)) {
+			(*this)[from]._edges.remove_if([&](Edge& x) { return _graph[x._vert]._value == to; });
+			return true;
+		}
+		return false;
+	};
+	bool remove_edge(V from, V to, Distance val) {
+		if (has_vertex(from)) {
+			(*this)[from]._edges.remove_if([&](Edge& x) { return (_graph[x._vert]._value == to && x._distance == val); });
+			return true;
+		}
+		return false;
+	};
+	bool has_edge(V from, V to, Distance val) {
+		if (has_vertex(from)) {
+			for (auto& edge : (*this)[from]._edges)
+				if (_graph[edge._num]._value == to && edge._value == val)
+					return true;
+		}
+		return false;
+	};
+	bool has_edge(V from, V to) {
+		if (has_vertex(from)) {
+			for (auto& edge : (*this)[from]._edge)
+				if (_graph[edge._num]._val == to)
+					return true;
+		}
+		return false;
+	};
+	list<Edge> edges(V from) const {
+		if (has_vertex(from))
+			return (*this)[from]._edge;
+	}
+
+	size_t order() const {
+		return _graph.size();
+	};
+	size_t degree(V val) {
+		return (*this)[val]._edge.size();
+	}
+
+	std::vector<Edge> shortest_path(const Vertex& from, const Vertex& to) const {
+
+	};
+	std::vector<Vertex> walk(const Vertex& start_vertex)const {
+
+	};
+};
--- task_tests.cpp
+++ task_tests.cpp
@@ -2,3 +2,18 @@
 #include <gtest/gtest.h>
 #include <stdexcept>
 #include <iostream>
+TEST(GraphTests, VertexCheck) {
+	Graph<int> g;
+	g.size();
+	g.add_vertex(5);
+	g.add_vertex(7);
+	g.add_vertex(9);
+	g.add_edge(5, 7, 10);
+	g.add_edge(5, 9, 20);
+	g.add_edge(7, 9, 30);
+	g.print();
+	cout << endl;
+	g.remove_vertex(7);
+	g.remove_edge(5, 9, 20);
+	g.print();
+};
